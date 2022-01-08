88c578b088a26229cf60902263ed0d09312b12ab
unknown <andresirias02@gmail.com>
2022-01-08 00:21:00 -0600

    Fix error

--- api/routes/users.js
+++ api/routes/users.js
@@ -59,17 +59,20 @@ router.get("/find/:id", async (req, res) => {
 //GET ALL
 router.get("/", verify, async (req, res) => {
   const query = req.query.new;
-  if (req.user.isAdmin) {
-    try {
-      const users = query
-        ? await User.find().sort({ _id: -1 }).limit(5)
-        : await User.find();
-      res.status(200).json(users);
-    } catch (err) {
-      res.status(500).json(err);
+  // if req user is not null
+  if (req.user) {
+    if (req.user.isAdmin) {
+      try {
+        const users = query
+          ? await User.find().sort({ _id: -1 }).limit(5)
+          : await User.find();
+        res.status(200).json(users);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } else {
+      res.status(403).json("You are not allowed to see all users!");
     }
-  } else {
-    res.status(403).json("You are not allowed to see all users!");
   }
 });
 
