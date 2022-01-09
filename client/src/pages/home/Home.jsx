a388b9fceb68027553ac0bd5fc41026a635b202d
unknown <andresirias02@gmail.com>
2022-01-09 17:45:45 -0600

    Change folder names

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,45 +1,66 @@
-import Navbar from "../../components/navbar/Navbar";
-import Featured from "../../components/featured/Featured";
-import "./home.scss";
-import List from "../../components/list/List";
-import { useEffect, useState } from "react";
+import Chart from "../../components/chart/Chart";
+import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
+import "./home.css";
+import { userData } from "../../dummyData";
+import WidgetSm from "../../components/widgetSm/WidgetSm";
+import WidgetLg from "../../components/widgetLg/WidgetLg";
+import { useEffect, useMemo, useState } from "react";
 import axios from "axios";
 
-const Home = ({ type }) => {
-  const [lists, setLists] = useState([]);
-  const [genre, setGenre] = useState(null);
+export default function Home() {
+  const MONTHS = useMemo(
+    () => [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Agu",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+    []
+  );
+
+  const [userStats, setUserStats] = useState([]);
 
   useEffect(() => {
-    const getRandomLists = async () => {
+    const getStats = async () => {
       try {
-        const res = await axios.get(
-          `lists${type ? "?type=" + type : ""}${
-            genre ? "&genre=" + genre : ""
-          }`,
-          {
-            headers: {
-              token:
-              "Bearer "+JSON.parse(localStorage.getItem("user")).accessToken,
-            },
-          }
+        const res = await axios.get("/users/stats", {
+          headers: {
+            token:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwZTZmYzQ2NDk0Mjc3MTYwNDg4MmMxNiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYyNTgzMjMxMSwiZXhwIjoxNjI2MjY0MzExfQ.ATXV-1TTWIGyVBttTQSf0erRWjsgZ8jHQv1ZsUixbng",
+          },
+        });
+        const statsList = res.data.sort(function (a, b) {
+          return a._id - b._id;
+        });
+        statsList.map((item) =>
+          setUserStats((prev) => [
+            ...prev,
+            { name: MONTHS[item._id - 1], "New User": item.total },
+          ])
         );
-        setLists(res.data);
       } catch (err) {
         console.log(err);
       }
     };
-    getRandomLists();
-  }, [type, genre]);
+    getStats();
+  }, [MONTHS]);
 
   return (
     <div className="home">
-      <Navbar />
-      <Featured type={type} setGenre={setGenre} />
-      {lists.map((list) => (
-        <List list={list} />
-      ))}
+      <FeaturedInfo />
+      <Chart data={userStats} title="User Analytics" grid dataKey="New User" />
+      <div className="homeWidgets">
+        <WidgetSm />
+        <WidgetLg />
+      </div>
     </div>
   );
-};
-
-export default Home;
+}
