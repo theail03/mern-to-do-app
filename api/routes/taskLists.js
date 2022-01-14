0071af14ae6e6f54d80e42d6c43edcf7756c0953
unknown <andresirias02@gmail.com>
2022-01-13 21:40:48 -0600

    Tasks and TaskLists CRUD

--- api/routes/taskLists.js
+++ api/routes/taskLists.js
@@ -2,14 +2,80 @@ const router = require("express").Router();
 const TaskList = require("../models/TaskList");
 const verify = require("../verifyToken");
 
-// create
-router.post("/", async (req, res) => {
-    const newTaskList = new TaskList(req.body);
-    try {
-        const savedTaskList = await newTaskList.save();
-        res.status(201).json(savedTaskList);
-    } catch (err) {
-        res.status(500).json(err);
+// CREATE
+router.post("/", verify, async (req, res) => {
+    if (req.user) {
+        const newTaskList = new TaskList(req.body);
+        try {
+            const savedTaskList = await newTaskList.save();
+            res.status(201).json(savedTaskList);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json("You are not allowed!");
+    }
+});
+
+// UPDATE
+router.put("/:id", verify, async (req, res) => {
+    if (req.user) {
+        try {
+            const updatedTaskList = await TaskList.findByIdAndUpdate(
+                req.params.id,
+                {
+                    $set: req.body,
+                },
+                { new: true }
+            );
+            res.status(200).json(updatedTaskList);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json("You are not allowed!");
+    }
+});
+  
+// DELETE
+router.delete("/:id", verify, async (req, res) => {
+    if (req.user) {
+        try {
+            await TaskList.findByIdAndDelete(req.params.id);
+            res.status(200).json("The taskList has been deleted...");
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json("You are not allowed!");
+    }
+});
+  
+// GET
+router.get("/:id", verify, async (req, res) => {
+    if (req.user) {
+        try {
+            const taskList = await TaskList.findById(req.params.id);
+            res.status(200).json(taskList);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json("You are not allowed!");
+    }
+});
+
+// GET ALL
+router.get("/", verify, async (req, res) => {
+    if (req.user) {
+        try {
+            const taskLists = await TaskList.find();
+            res.status(200).json(taskLists.reverse());
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json("You are not allowed!");
     }
 });
 
--- api/routes/tasks.js
+++ api/routes/tasks.js
@@ -2,14 +2,80 @@ const router = require("express").Router();
 const Task = require("../models/Task");
 const verify = require("../verifyToken");
 
-// create
-router.post("/", async (req, res) => {
-    const newTask = new Task(req.body);
-    try {
-        const savedTask = await newTask.save();
-        res.status(201).json(savedTask);
-    } catch (err) {
-        res.status(500).json(err);
+// CREATE
+router.post("/", verify, async (req, res) => {
+    if (req.user) {
+        const newTask = new Task(req.body);
+        try {
+            const savedTask = await newTask.save();
+            res.status(201).json(savedTask);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json("You are not allowed!");
+    }
+});
+
+// UPDATE
+router.put("/:id", verify, async (req, res) => {
+    if (req.user) {
+        try {
+            const updatedTask = await Task.findByIdAndUpdate(
+                req.params.id,
+                {
+                    $set: req.body,
+                },
+                { new: true }
+            );
+            res.status(200).json(updatedTask);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json("You are not allowed!");
+    }
+});
+  
+// DELETE
+router.delete("/:id", verify, async (req, res) => {
+    if (req.user) {
+        try {
+            await Task.findByIdAndDelete(req.params.id);
+            res.status(200).json("The task has been deleted...");
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json("You are not allowed!");
+    }
+});
+  
+// GET
+router.get("/:id", verify, async (req, res) => {
+    if (req.user) {
+        try {
+            const task = await Task.findById(req.params.id);
+            res.status(200).json(task);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json("You are not allowed!");
+    }
+});
+
+// GET ALL
+router.get("/", verify, async (req, res) => {
+    if (req.user) {
+        try {
+            const task = await Task.find();
+            res.status(200).json(task.reverse());
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json("You are not allowed!");
     }
 });
 
