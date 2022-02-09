d055ca98b646f269c8aebc58fc8a04518109d380
unknown <andresirias02@gmail.com>
2022-02-08 19:34:30 -0600

    Show custom fields in table

--- client/src/pages/taskTable/TaskTable.jsx
+++ client/src/pages/taskTable/TaskTable.jsx
@@ -2,23 +2,69 @@ import "./taskTable.css";
 import { DataGrid } from "@material-ui/data-grid";
 import { DeleteOutline } from "@material-ui/icons";
 import { Link, useParams } from "react-router-dom";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { TaskContext } from "../../context/taskContext/TaskContext";
+import { TaskListContext } from "../../context/taskListContext/TaskListContext";
 import { deleteTask, getTasks } from "../../context/taskContext/apiCalls";
+import { getTaskList } from "../../context/taskListContext/apiCalls";
 
 export default function TaskTable() {
   const { tasks, dispatch } = useContext(TaskContext);
   const { taskListId } = useParams();
+  const { taskLists, dispatch: dispatchTaskLists } = useContext(TaskListContext);
+  const [ taskList, setTaskList ] = useState([]);
+  const [ tableColumns, setTableColumns ] = useState([]);
+
+  const transformCustomFields = (task) => {
+    task.customFields.forEach(customField => {
+      // append custom field to task
+      task[customField.id] = customField.value;
+    });
+    return task;
+  }
 
   useEffect(() => {
     getTasks(dispatch, taskListId);
   }, [dispatch, taskListId]);
 
+  useEffect(() => {
+    getTaskList(dispatchTaskLists, taskListId);
+  }, []);
+
+  useEffect(() => {
+    if (taskLists) {
+      const taskList = taskLists[0];
+      setTaskList(taskList);
+    }
+  }, [taskLists]);
+
+  useEffect(() => { 
+    if (taskList) {
+      // transform custom fields into a format that DataGrid can use
+      if (taskList.customFields) {
+        const customFields = taskList.customFields.map(customField => {
+          return {
+            field: customField.id,
+            headerName: customField.name,
+            width: 200
+          };
+        });
+        // set the columns for the DataGrid based on default columns and custom fields
+        setTableColumns([
+          ...defaultColumns,
+          ...customFields
+        ]);
+      }
+    }
+  }, [taskList]);
+
   const handleDelete = (id) => {
+    console.log(tasks);
+    window.confirm("Are you sure you want to delete this task?") &&
     deleteTask(id, dispatch);
   };
 
-  const columns = [
+  const defaultColumns = [
     { field: "_id", headerName: "ID", width: 90 },
     { field: "title", headerName: "Title", width: 150 },
     {
@@ -46,9 +92,9 @@ export default function TaskTable() {
   return (
     <div className="taskTable">
       <DataGrid
-        rows={tasks}
+        rows={tasks.map(transformCustomFields)}
         disableSelectionOnClick
-        columns={columns}
+        columns={tableColumns}
         pageSize={8}
         checkboxSelection
         getRowId={(r) => r._id}
