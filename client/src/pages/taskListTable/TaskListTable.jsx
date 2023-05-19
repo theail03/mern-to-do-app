a9f54ae8c7472e5ce3711ab4917e01d5155da6e7
andresirias03 <andresirias03@gmail.com>
2023-05-19 13:38:32 -0600

    Export functionality for a single task list

--- client/src/pages/taskListTable/TaskListTable.jsx
+++ client/src/pages/taskListTable/TaskListTable.jsx
@@ -3,12 +3,18 @@ import "./taskListTable.css";
 import { DataGrid } from "@material-ui/data-grid";
 import { DeleteOutline } from "@material-ui/icons";
 import { Link } from "react-router-dom";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { TaskListContext } from "../../context/taskListContext/TaskListContext";
 import { deleteTaskList, getTaskLists } from "../../context/taskListContext/apiCalls";
+import * as XLSX from 'xlsx';
+import { TaskContext } from "../../context/taskContext/TaskContext";
+import { getTasks } from "../../context/taskContext/apiCalls";
 
 export default function TaskListTable() {
   const { taskLists, dispatch } = useContext(TaskListContext);
+  const { tasks: exportTasks, dispatch: dispatchExportTasks } = useContext(TaskContext);
+  const [ exporting, setExporting ] = useState(false);
+  const [ exportTaskListId, setExportTaskListId ] = useState(null);
 
   useEffect(() => {
     getTaskLists(dispatch);
@@ -18,6 +24,43 @@ export default function TaskListTable() {
     window.confirm("Are you sure you want to delete this task list?") &&
     deleteTaskList(id, dispatch);
   };
+
+  const handleExport = async (id) => {
+    if (window.confirm("Are you sure you want to export this task list?")) {
+      await getTasks(dispatchExportTasks, id);
+      setExportTaskListId(id);
+      setExporting(true); 
+    }
+  };
+
+  useEffect(() => {
+    if (exporting) {
+      const taskList = taskLists.find(taskList => taskList._id === exportTaskListId); 
+      const taskRows = exportTasks.map(task => { 
+        const row = { title: task.title };
+
+        /* get tag names from taskList */
+        row.tags = task.tags.map(taskTag => 
+          taskList.tags.find(taskListTag => taskTag === taskListTag.id).tag).join(";");
+
+        taskList.customFields.forEach(cf => {
+          row[cf.name] = task.customFields.find(tcf => tcf.id === cf.id).value;
+        });
+        return row;
+      });
+
+      /* Info from https://docs.sheetjs.com/docs/getting-started/example/ */
+
+      /* generate worksheet and workbook */
+      const worksheet = XLSX.utils.json_to_sheet(taskRows);
+      const workbook = XLSX.utils.book_new();
+      XLSX.utils.book_append_sheet(workbook, worksheet, taskList.title);
+
+      /* create an XLSX file */
+      XLSX.writeFile(workbook, taskList.title + Date.now() + ".xlsx", { compression: true });
+      setExporting(false);
+    }
+  }, [exporting]);
 
   const columns = [
     { field: "_id", headerName: "ID", width: 190 },
@@ -34,7 +77,7 @@ export default function TaskListTable() {
             >
               <button className="taskTableSeeTasks">See tasks</button>
             </Link>
-            <button className="taskTableExport">Export</button>
+            <button className="taskTableExport" onClick={() => handleExport(params.row._id)}>Export</button>
             <Link
               to={{ pathname: "/taskList/" + params.row._id }}
             >
