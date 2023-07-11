b474c8e00dd0314ba2061c3368dc6f99932c771d
theail03 <andresirias03@gmail.com>
2023-07-11 14:41:32 -0600

    Reset session config

--- api/index.js
+++ api/index.js
@@ -39,7 +39,11 @@ app.use(
   session({
     secret: process.env.SECRET_KEY,
     resave: true,
-    saveUninitialized: true
+    saveUninitialized: false,
+    cookie: {
+      sameSite: 'none',
+      secure: true
+    }
   })
 )
 
