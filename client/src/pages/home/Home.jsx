d8586b7e05316fd998d44cf3f0d0bc514d95cc79
theail03 <andresirias03@gmail.com>
2024-03-03 22:29:26 -0600

    Create layout for home page

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,10 +1,42 @@
 import { Page } from "../../styles/Page.styled";
+import { Box, BoxTitle, ResponsiveRow, Column, BoxContent } from "./Home.styled";
 
 export default function Home() {  
 
   return (
     <Page>
-      <h1>Welcome</h1>
+      <Column>
+        <h1>Welcome</h1>
+        <Box>
+          <BoxTitle>Title</BoxTitle>
+          <BoxContent>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </BoxContent>
+        </Box>
+        <ResponsiveRow>
+          <Box>
+            <BoxTitle>Title</BoxTitle>
+            <BoxContent>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </BoxContent>
+          </Box>
+          <Box>
+            <BoxTitle>Title</BoxTitle>
+            <BoxContent>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </BoxContent>
+          </Box>
+        </ResponsiveRow>
+        <ResponsiveRow>
+          <Box>
+            <BoxTitle>Title</BoxTitle>
+            <BoxContent>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </BoxContent>
+          </Box>
+          <Box>
+            <BoxTitle>Title</BoxTitle>
+            <BoxContent>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </BoxContent>
+          </Box>
+        </ResponsiveRow>
+      </Column>
     </Page>
   );
 }
